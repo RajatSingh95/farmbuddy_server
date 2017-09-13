5ae56243eb794e7642da5f4d2e7b7fc14534b164
RajatSingh95 <rajat.s15@iiits.in>
2017-09-13 11:44:53 -0700

    Update models.py

--- restapi/models.py
+++ restapi/models.py
@@ -1,7 +1,25 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
+import datetime
 
 # Create your models here.
+class Houses(models.Model):
+	HID=models.AutoField(primary_key=True)
+	point=models.PointField(default=Point(1,1),null=True)
+
+class Farms(models.Model):
+	FID=models.AutoField(primary_key=True)
+	plot=models.PolygonField(srid=4326,geography=True)
+	area=models.FloatField(default=0.0)
+	
+	def save(self):
+		temp=self.plot.transform(27700,clone=True)
+		self.area=temp.area
+		super().save(self)
+	
 class Farmer(models.Model):
+	FRID=models.AutoField(primary_key=True)
+	HID=models.ForeignKey(Houses,to_field='HID',on_delete=models.CASCADE)
 	name = models.CharField(max_length=250)
 	dob = models.DateField(max_length=250)
 	address = models.CharField(max_length=550)
@@ -9,6 +27,9 @@ class Farmer(models.Model):
 	password = models.CharField(max_length=250)
 
 class Landlord(models.Model):
+	LID=models.AutoField(primary_key=True)
+	HID=models.ForeignKey(Houses,to_field='HID',on_delete=models.CASCADE)
+	FID=models.ForeignKey(Farms,to_field='FID',on_delete=models.CASCADE)
 	name = models.CharField(max_length=250)
 	dob = models.DateField(max_length=250)
 	address = models.CharField(max_length=550)
@@ -25,5 +46,6 @@ class Bid(models.Model):
 	price = models.BigIntegerField()
 	farmer_id = models.IntegerField()
 	advertisement_id = models.IntegerField()
+
 
 
