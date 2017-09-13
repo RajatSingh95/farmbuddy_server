5823b8adece85201a00689dbaa2e65521113bbbd
RajatSingh95 <rajat.s15@iiits.in>
2017-09-13 15:03:06 -0700

    Update urls.py

--- restapi/urls.py
+++ restapi/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     url( r'^$' , views.index , name='index' ),
     url( r'^farmerdetail/' , views.farmerdetail , name='farmerdetail' ),
     url( r'^mapdetail/' , views.mapdetail , name='mapdetail' ),
+    url( r'^logindetail/' , views.logindetail , name='logindetail' ),
+
 ]
+
 
