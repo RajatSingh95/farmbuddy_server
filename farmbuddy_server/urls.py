d5f2c13f67931b6c7bf6191d8b205b340eb3eea8
RajatSingh95 <rajat.s15@iiits.in>
2017-10-06 06:56:37 -0700

    hello

--- farmbuddy_server/urls.py
+++ farmbuddy_server/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^restapi/', include('restapi.urls')),
