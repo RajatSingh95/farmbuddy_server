fab5bb8c0dcc1a329882b2548005945da55924a5
RajatSingh95 <rajat.s15@iiits.in>
2017-10-06 03:13:44 -0700

    media folder

--- restapi/models.py
+++ restapi/models.py
@@ -7,12 +7,17 @@ class Houses(models.Model):
 	HID=models.AutoField(primary_key=True)
 	point=models.PointField(default=Point(1,1),null=True)
 	file=models.FileField(upload_to = 'images/houses/',null=True)
+	def __str__(self):
+		return "House : %s" %(self.HID)
+
 
 class Farms(models.Model):
 	FID=models.AutoField(primary_key=True)
 	plot=models.PolygonField(srid=4326,geography=True)
 	area=models.FloatField(default=0.0)
 	file=models.FileField(upload_to = 'images/farms/',null=True)
+	def __str__(self):
+		return "%s : %s" %(self.FID,self.area)
 	
 	def save(self):
 		temp=self.plot.transform(27700,clone=True)
@@ -24,6 +29,8 @@ class Wells(models.Model):
 	point=models.PointField(default=Point(1,1),null=True)
 	depth=models.FloatField(default=0.0)
 	file=models.FileField(upload_to = 'images/wells/',null=True)
+	def __str__(self):
+		return "%s : %s" %(self.WID,self.depth)
 
 class Crops(models.Model):
 	Name=models.CharField(max_length=50,default="Rice")
@@ -31,7 +38,8 @@ class Crops(models.Model):
 	Year=models.IntegerField()
 	seasons=(('S',"Summer"),('W',"Winter"),('M',"Monsoon"))
 	Seasons=models.CharField(max_length=20,choices=seasons)
-
+	def __str__(self):
+		return "%s : %s" %(self.Name,self.Seasons)
 	
 class Farmer(models.Model):
 	HID=models.ForeignKey(Houses,to_field='HID',on_delete=models.CASCADE)
@@ -40,6 +48,8 @@ class Farmer(models.Model):
 	address = models.CharField(max_length=550)
 	mobile_no = models.CharField(max_length=250)
 	password = models.CharField(max_length=250)
+	def __str__(self):
+		return "%s : %s" %(self.HID,self.name)
 
 class Landlord(models.Model):
 	HID=models.ForeignKey(Houses,to_field='HID',on_delete=models.CASCADE)
@@ -49,23 +59,29 @@ class Landlord(models.Model):
 	address = models.CharField(max_length=550)
 	mobile_no = models.CharField(max_length=250)
 	password = models.CharField(max_length=250)
+	def __str__(self):
+		return "Landlord %s : %s" %(self.FID,self.name)
 
 class Advertisement(models.Model):
 	landlord_id = models.IntegerField()
 	land_price = models.BigIntegerField()
 	leased_to = models.IntegerField() #farmer_id
 	bid_id = models.IntegerField() #bid given to the particular land
+	def __str__(self):
+		return "%s : %s" %(self.landlord_id,self.leased_to)
 
 class Bid(models.Model):
 	price = models.BigIntegerField()
 	farmer_id = models.IntegerField()
 	advertisement_id = models.IntegerField()
+	def __str__(self):
+		return "%s : %s" %(self.advertisement_id,self.price)
 
 class Yields(models.Model):
 	WID=models.ForeignKey(Wells,to_field='WID',on_delete=models.CASCADE)
 	Yield=models.FloatField(default=0.0)
 	measured_date=models.DateField(default=datetime.date.today)
 	def __str__(self):
-		return "%s : %s" %(self.WID,self.WID)
+		return "%s : %s" %(self.WID,self.Yield)
 
 
